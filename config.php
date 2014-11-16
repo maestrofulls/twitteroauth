b211c4eb8f84948292a2533935464ede55d2d4f9
maestrofulls <mr.fulls@gmail.com>
2014-11-17 06:15:22 +0700

    Update and rename config-sample.php to config.php

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @file
+ * A single location to store configuration.
+ */
+
+define('CONSUMER_KEY', 'ln2pDWsmOpDKXGaCG6j7Pxs14');
+define('CONSUMER_SECRET', 'PtRRx0cYN2MjnD7GRgaP4Uj1jtQZssbOY3DiMJfulOiCpTI9Mx');
+define('OAUTH_CALLBACK', 'http://okeboss.biz/twitteroauth/callback.php');
